24b18e8b958ca261cac0d3a0bd8b0e4632e67d89
hectapia <oli21007@byui.edu>
2022-09-17 13:10:29 -0500

    WDD 230: Assignment Portal

---
+++ scripts/scripting.js
@@ -0,0 +1,5 @@
+
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById('currentyear').innerHTML = year;
+document.getElementById('lastupdated').textContent = `Last Updated: ${document.lastModified}`;
