1b865b51351d39ef3f661c9a230b02d9502a3a91
comimsaurs <holivares@comimsa.com>
2022-10-12 00:51:07 -0500

    Chamber project started

---
+++ chamber/scripts/scripting.js
@@ -0,0 +1,44 @@
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+    if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+
+  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  */
+
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+//const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+//datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
+
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById('currentyear').innerHTML = year;
+document.getElementById('lastupdated').textContent = `Last Updated: ${document.lastModified}`;
